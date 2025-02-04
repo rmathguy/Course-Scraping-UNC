b757ea12565d400b2163b195f61224bd029fc244
rmathguy <rob.c.stern@gmail.com>
2025-02-04 15:52:32 -0500

    Made the simpler version the main version for the get_semester function

--- get_semester.py
+++ get_semester.py
@@ -1,35 +1,8 @@
-import tkinter as tk
+import easygui
 
 
 def get_Semester():
-
-    frame = tk.Tk()
-    frame.title("TextBox Input")
-    frame.geometry('400x200')
-# Function for getting Input
-# from textbox and printing it
-# at label widget
-    def Input(event):
-        global semesterPASS
-        semesterPASS = inputtxt.get(1.0, "end-1c")
-        inputtxt.quit()
-
-
-# TextBox Creation
-    inputtxt = tk.Text(frame,
-                       height=5,
-                       width=20)
-
-    inputtxt.pack()
-    inputtxt.bind('<Return>', Input)
-# Button Creation
-    Button = tk.Button(frame,
-                       text="Submit")
-    Button.pack()
-    Button.bind('<Button-1>', Input)
-# Label Creation
-    lbl = tk.Label(frame, text="Which Semester?")
-    lbl.pack()
-    frame.mainloop()
-    frame.destroy()
-    return (semesterPASS)
+    semesterPass = easygui.enterbox("Which Semester Formmated as 'YYYY Season'",
+                                    "Semester Prompt", "")
+    return(semesterPass)
+ 
