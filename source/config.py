62d57b6a296926e875360c83c634c0465c8f3138
rmathguy <rob.c.stern@gmail.com>
2025-02-24 00:24:00 -0500

    fixed some of the config stuff to save files properly

--- source/config.py
+++ source/config.py
@@ -1,11 +1,11 @@
-import easygui # For Graphical Pop-ups
-import configparser # For Config file handling.
+import easygui  # For Graphical Pop-ups
+import configparser  # For Config file handling.
 
 
 config = configparser.ConfigParser()
 
 
-''' 
+'''
 ======================
     TODO
 Author: rmathguy
@@ -54,10 +54,11 @@ def get_config():
 
 
 def save_config():
-    config_file = easygui.fileopenbox(
-            msg="Choose where to save the configuration file.",
-            filetypes=["*.cfg"],
-            default="*.cfg")
+    config_file = easygui.filesavebox(
+                    msg="Choose where to save the configuration file.",
+                    filetypes=["*.cfg"],
+                    default="*.cfg"
+                    )
 
     with open(config_file, 'w') as configfile:
         config.write(configfile)
